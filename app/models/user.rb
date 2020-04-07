43d724a403fc416ed641d6698699aad035a2b296
今枝光 <hikaru.fc0710@gmail.com>
2020-04-07 14:25:20 +0000

    [追加]github認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,23 +9,28 @@
 #  image                  :string
 #  introduction           :text
 #  name                   :string           not null
+#  provider               :string           default(""), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  status                 :string           not null
+#  uid                    :string           default(""), not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
 #  index_users_on_email                 (email) UNIQUE
+#  index_users_on_provider_and_uid      (provider,uid) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i(github)
+
   extend Enumerize
 
   validates :name, presence: true
@@ -36,4 +41,28 @@ class User < ApplicationRecord
 
   enumerize :status, in: %i[engineer beginner]
   has_many :books, dependent: :destroy
+
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    unless user
+      user = User.new(provider: auth.provider,
+                      uid:      auth.uid,
+                      name:     auth.info.name,
+                      email:    User.dummy_email(auth),
+                      password: Devise.friendly_token[0, 20]
+      )
+    end
+    user.save
+    user
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_103107) do
+ActiveRecord::Schema.define(version: 2020_04_07_134247) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
@@ -63,7 +63,10 @@ ActiveRecord::Schema.define(version: 2020_04_07_103107) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
