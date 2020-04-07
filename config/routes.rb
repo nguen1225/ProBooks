c4ed3262a3849a23f26ddb47f5a72b8c50448978
今枝光 <hikaru.fc0710@gmail.com>
2020-04-07 10:50:52 +0000

    [追加]書籍のタグ機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
   resources :books
+  get 'tags/:tag', to: 'books#index', as: :tag
 end
