ea946ff5833f39fadaf19a1d11f9c1d04b2f2a93
今枝光 <>
2020-04-13 16:23:24 +0900

    migrateionファイル修正

--- db/migrate/20200407103103_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20200407103103_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -18,7 +18,7 @@ AddMissingUniqueIndices.class_eval do
   def self.down
     # remove_index ActsAsTaggableOn.tags_table, :name
 
-    remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_idx'
+    # remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_idx'
 
     add_index ActsAsTaggableOn.taggings_table, :tag_id unless index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
     add_index ActsAsTaggableOn.taggings_table, [:taggable_id, :taggable_type, :context], name: 'taggings_taggable_context_idx'
