9fb56887d7a5cfb56a0f9e58f732e0629b1c6f01
今枝光 <>
2020-04-13 16:33:00 +0900

    migrateionファイル修正

--- db/migrate/20200409062617_create_claps.rb
+++ db/migrate/20200409062617_create_claps.rb
@@ -1,10 +1,11 @@
 class CreateClaps < ActiveRecord::Migration[5.2]
   def change
     create_table :claps do |t|
-      t.references :user, foreign_key: true
-      t.references :review, foreign_key: true
+      t.references :user
+      t.references :review
 
       t.timestamps
     end
+    add_foreign_key :user, :review
   end
 end
