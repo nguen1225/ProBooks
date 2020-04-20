5212b50d35475ed23c2893c4f988c4bb2b0c29b1
今枝光 <hikaru.fc0710@gmail.com>
2020-04-20 14:44:00 +0000

    [追加]fontawesome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'アカウント情報を更新しました'
+    else
+      render :edit
+    end
+  end
 
   def show
+    #レビューをした回数
     @user_reviews_count = Review.where(user_id: @user.id).size
     #拍手をもらった回数
     @user_craps_count = Clap.where(review_id: @user.reviews.ids).size
+    #拍手を送った回数
     @user_post_claps = Clap.where(user_id: @user.id).count
+    #書籍の投稿数
     @user_post_books = @user.books.size
   end
 
