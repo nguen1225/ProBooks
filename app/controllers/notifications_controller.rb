6cb1e74cd47d84563764ed03aa44d52d0b4e486f
今枝光 <hikaru.fc0710@gmail.com>
2020-04-24 12:05:08 +0000

    [追加]ログインチェック

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @notifications = current_user.passive_notifications.includes(:visitor, :visited, :book).page(params[:page]).per(5)
     @notifications.where(checked: false).each do |notification|
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :authenticate_user!, only: %i[edit show]
 
   def edit; end
 
