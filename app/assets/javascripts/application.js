2c9526effd51ea384fbcbc92913864fe699108bd
今枝光 <hikaru.fc0710@gmail.com>
2020-04-24 14:25:28 +0000

    [修正]jsの記述場所の変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,31 +29,24 @@ $(document).ready(function(){
 });
 
 //top_page tabs
-$(document).ready(function(){
-	$('.tabs').tabs();
-});
+// $(document).ready(function(){
+// 	$('.tabs').tabs();
+// });
 
-// top_page image
-$(document).ready(function(){
-	$('.materialboxed').materialbox();
-});
+// // top_page image
+// $(document).ready(function(){
+// 	$('.materialboxed').materialbox();
+// });
 
-//review modal
-$(document).ready(function(){
-    $('.modal').modal();
-});
 
 //user index admin
-$(document).ready(function(){
-	$('.datepicker').datepicker({
-		i18n:{
-			months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-			weekdays: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-        },
-        format: "yyyy-mm-dd"
-	});
-});
+// $(document).ready(function(){
+// 	$('.datepicker').datepicker({
+// 		i18n:{
+// 			months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+// 			weekdays: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
+//         },
+//         format: "yyyy-mm-dd"
+// 	});
+// });
 
-$(document).ready(function(){
-	$('.tooltipped').tooltip();
-});
