b9b02b4b41eccaefa394e44f1b70017fd366ce4a
今枝光 <hikaru.fc0710@gmail.com>
2020-04-24 19:23:20 +0000

    [修正]js記述場所

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,35 +18,43 @@
 //= require activestorage
 //= require_tree .
 
-//simple_form select
+//ほぼ全て
 $(document).ready(function(){
 	$('select').formSelect();
 });
 
-
+//app all
 $(document).ready(function(){
 	$('.sidenav').sidenav();
 });
 
-//top_page tabs
-// $(document).ready(function(){
-// 	$('.tabs').tabs();
-// });
+//book index tabs
+$(document).ready(function(){
+	$('.tabs').tabs();
+});
 
 // // top_page image
-// $(document).ready(function(){
-// 	$('.materialboxed').materialbox();
-// });
+$(document).ready(function(){
+	$('.materialboxed').materialbox();
+});
 
 
 //user index admin
-// $(document).ready(function(){
-// 	$('.datepicker').datepicker({
-// 		i18n:{
-// 			months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-// 			weekdays: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-//         },
-//         format: "yyyy-mm-dd"
-// 	});
-// });
+$(document).ready(function(){
+	$('.datepicker').datepicker({
+		i18n:{
+			months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+			weekdays: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
+        },
+        format: "yyyy-mm-dd"
+	});
+});
+
+
+//ツールチップ
+//レビューモーダル
+$(document).ready(function(){
+	$('.tooltipped').tooltip();
+	$('.modal').modal();
+});
 
