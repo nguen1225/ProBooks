26c6484b94e6fa7882dbd0f670244bcd05fa11b9
今枝光 <hikaru.fc0710@gmail.com>
2020-04-24 20:02:29 +0000

    [追加]AdminのUsersとBooksのrequestSpec

---
+++ spec/requests/admin_books_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "AdminBooks", type: :request do
+  describe "GET #index" do
+    let(:admin_user) { FactoryBot.create :admin}
+    let!(:book_a) { FactoryBot.create :book }
+
+    context '管理者である場合' do
+      before do
+          sign_in admin_user
+      end
+      it 'リクエストが成功すること' do
+        get admins_books_url
+        expect(response.status).to eq 200
+      end
+      it '書籍名が表示されていること' do
+        get admins_books_url
+        expect(response.body).to include book_a.title
+      end
+      end
+      it '難易度が表示されていること' do
+        get admins_books_url
+        expect(response.body).to include book_a.level
+      end
+    end
+    context '管理者でない場合' do
+      it "リダイレクトすること" do
+        get admins_books_url
+        expect(response).to redirect_to new_admin_session_path
+      end
+    end
+  end
+end
