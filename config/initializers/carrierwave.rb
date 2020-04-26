cc2d868d8902db3ce9bd4fea01857eb622232b6a
今枝光 <hikaru.fc0710@gmail.com>
2020-04-26 10:30:03 +0000

    AWS画像アップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     config.storage :fog
 	config.fog_provider = 'fog/aws'
 	config.fog_directory  = 'tumiagebaket'
-	config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tumiagebaket'
+	config.asset_host = 'https://tumiagebaket.s3-ap-northeast-1.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
