ec74b717442854652c848a20bb97a3d6865601a3
今枝光 <hikaru.fc0710@gmail.com>
2020-04-26 10:38:27 +0000

    AWS画像アップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+	  use_iam_profile: true,
 	  region: 'ap-northeast-1'
 	}
 end
