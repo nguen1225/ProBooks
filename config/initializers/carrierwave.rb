2794ec101ea8a916c0ef554888306967184c020a
今枝光 <hikaru.fc0710@gmail.com>
2020-04-26 13:00:49 +0000

    AWS画像アップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    config.storage :fog
 	config.fog_provider = 'fog/aws'
-	config.fog_directory  = 'tumiagebaket'
-	config.asset_host = 'https://tumiagebaket.s3-ap-northeast-1.amazonaws.com'
+	config.fog_directory  = 'probooksbucket'
+	config.asset_host = 'https://probooksbucket.s3-ap-northeast-1.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
-	  use_iam_profile: true,
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 	  region: 'ap-northeast-1'
 	}
+    config.enable_processing = false if Rails.env.test?
 end
