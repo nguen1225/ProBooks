f8384488886370888d50e1fea268516d63cfd03c
今枝光 <hikaru.fc0710@gmail.com>
2020-04-26 06:14:31 +0000

    []

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -3,12 +3,10 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
+  if Rails.env.production? || Rails.env.staging?
     storage :fog
+  else
+    storage :file
   end
 
   version :retina do
@@ -16,7 +14,6 @@ class ImagesUploader < CarrierWave::Uploader::Base
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
