a8873f991dbb76e64b0ba974f0dfa7c3cb206899
今枝光 <hikaru.fc0710@gmail.com>
2020-04-26 11:17:04 +0000

    AWS画像アップロード

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,9 +44,3 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 #--------画像追加のために追加したコー
 
 # 必要に応じて/環境変数をcapistranoでの自動デプロイで利用
-set :default_env, {
-	rbenv_root: "/usr/local/rbenv",
-	path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-	AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-	AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
